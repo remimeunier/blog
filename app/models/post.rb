05a8409f654f984ec1bed6920a98ad5f57525746
remimeunier <remi.meunier@insa-lyon.fr>
2013-05-16 20:14:30 +0200

    integrate vidéos

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,3 @@
 class Post < ActiveRecord::Base
-  attr_accessible :content, :title, :created_at
+  attr_accessible :content, :title, :created_at, :link
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130516172300) do
+ActiveRecord::Schema.define(:version => 20130516175827) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "link"
   end
 
 end
