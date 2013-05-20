bd89bc6c2d57d3ead86c3f81afc93562b3beab99
remimeunier <remi.meunier@insa-lyon.fr>
2013-05-20 19:34:28 +0200

    securite and  dimensionement

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,12 @@
 class Post < ActiveRecord::Base
   belongs_to :genre
   attr_accessible :content, :title, :created_at, :link, :genre_id, :image
+
+
+  	# voir la gem papercast avec imagemagic pour le redimmensionnement
   has_attached_file :image
+
+	validates_attachment_presence :image
+	validates_attachment_size :image, :less_than => 5.megabytes
+	validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/png', 'image/jpg']
 end
